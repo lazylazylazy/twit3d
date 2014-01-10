c03c586b7be61375172aabf27aadc6fdd62c05a3
Michael <michaelnicholasfisher@gmail.com>
2014-01-10 11:49:43 +0000

    initial commit - adds basic sinatra framework

---
+++ Gemfile
@@ -0,0 +1,20 @@
+source 'https://www.rubygems.org/'
+
+ruby '2.0.0'
+
+gem 'sinatra'
+# gem 'data_mapper'
+# gem 'dm-postgres-adapter'
+# gem 'bcrypt-ruby'
+# gem 'rack-flash3'
+# gem 'sinatra-partial'
+gem 'shotgun'
+
+group :test do
+
+gem 'rspec'
+gem 'capybara'
+gem 'jasmine'
+# gem 'database_cleaner'
+
+end
---
+++ app/server.rb
@@ -0,0 +1,24 @@
+require 'sinatra'
+# require 'data_mapper'
+# require 'rack-flash'
+# require 'sinatra/partial'
+# require_relative 'models/link'
+# require_relative 'models/tag'
+# require_relative 'models/user'
+# require_relative 'helpers/application'
+# require_relative 'data_mapper_setup'
+
+# require_relative 'controllers/users'
+# require_relative 'controllers/sessions'
+# require_relative 'controllers/links'
+# require_relative 'controllers/tags'
+# require_relative 'controllers/application'
+
+# enable :sessions
+# set :session_secret, 'super secret'
+# use Rack::Flash
+# set :partial_template_engine, :erb
+
+get "/" do
+	erb: layout
+end
